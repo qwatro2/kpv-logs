d77a57df16b5b9fea7423407ccd237106474823f
qwatro2 <kv22828@gmail.com>
2024-10-25 20:00:27 +0300

    refactor: rename PathValidator to Validator

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,13 +1,6 @@
 package backend.academy;
 
-import backend.academy.logs.file.FileGetter;
-import backend.academy.logs.file.LocalFileGetter;
-import backend.academy.logs.parsers.CommandLineParser;
-import backend.academy.logs.parsers.LogsCommandLineParser;
-import backend.academy.logs.entities.ParsingResult;
 import lombok.experimental.UtilityClass;
-import java.io.File;
-import java.util.List;
 
 @UtilityClass
 public class Main {
