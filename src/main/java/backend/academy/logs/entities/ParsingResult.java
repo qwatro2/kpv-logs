fa676415018b1ea58b9b53f4de48d790a8e1f755
qwatro2 <kv22828@gmail.com>
2024-10-31 01:52:38 +0300

    fit: add argument --output for redirect recording results to file

--- src/main/java/backend/academy/logs/entities/ParsingResult.java
+++ src/main/java/backend/academy/logs/entities/ParsingResult.java
@@ -7,22 +7,22 @@ import java.util.Objects;
 @Getter
 @Setter
 public class ParsingResult {
-    private boolean help;
     private String path;
     private String from;
     private String to;
     private String format;
     private String filterField;
     private String filterValue;
+    private String output;
 
     public static boolean equals(ParsingResult lhs, ParsingResult rhs) {
-        return lhs.help == rhs.help
-            && Objects.equals(lhs.path, rhs.path)
+        return Objects.equals(lhs.path, rhs.path)
             && Objects.equals(lhs.from, rhs.from)
             && Objects.equals(lhs.to, rhs.to)
             && Objects.equals(lhs.format, rhs.format)
             && Objects.equals(lhs.filterField, rhs.filterField)
-            && Objects.equals(lhs.filterValue, rhs.filterValue);
+            && Objects.equals(lhs.filterValue, rhs.filterValue)
+            && Objects.equals(lhs.output, rhs.output);
     }
 
     public boolean equals(ParsingResult other) {
