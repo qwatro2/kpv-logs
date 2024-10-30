60c8765940929b47bcede54bf4c88c0ac0b53fe7
qwatro2 <kv22828@gmail.com>
2024-10-30 23:21:47 +0300

    fit: add MarkdownNginxStatisticsPrinter implementation of NginxStatisticsPrinter

---
+++ src/main/java/backend/academy/logs/printers/MarkdownNginxStatisticsPrinter.java
@@ -0,0 +1,44 @@
+package backend.academy.logs.printers;
+
+import backend.academy.logs.converters.Converter;
+import backend.academy.logs.entities.ParsingResult;
+import backend.academy.logs.statistics.NginxStatistics;
+import backend.academy.logs.types.RequestType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class MarkdownNginxStatisticsPrinter extends NginxStatisticsPrinter {
+
+    public MarkdownNginxStatisticsPrinter(Converter<Integer> statusConverter, Converter<RequestType> requestTypeConverter) {
+        super(statusConverter, requestTypeConverter);
+    }
+
+    @Override
+    protected void printBaseInformation(ParsingResult parsingResult, NginxStatistics statistics) {
+        printStream.println("#### Общая информация");
+        printStream.println("|Метрика|Значение|");
+        printStream.println("|:-:|-:|");
+        printStream.println("|Файл(-ы)|" + renderList(statistics.names()) + "|");
+        printStream.println("|Начальная дата|" + renderString(parsingResult.from()) + "|");
+        printStream.println("|Конечная дата|" + renderString(parsingResult.to()) + "|");
+        printStream.println("|Количество запросов|" + statistics.numberOfRequests() + "|");
+        printStream.println("|Средний размер ответа|" + statistics.averageBodyBytesSent() + "|");
+        printStream.println("|95-перцентиль размера ответа|" + statistics.percentile() + "|");
+        printStream.println("|Самая старая запись|" + statistics.oldestLogTimestamp() + "|");
+        printStream.println("|Самая новая запись|" + statistics.newestLogTimestamp() + "|");
+    }
+
+    @Override
+    protected <K> void printMapCounter(String title, HashMap<K, Integer> mapCounter, Function<K, String> keyRenderer) {
+        printStream.println("#### " + title);
+        printStream.println("|" + title + "|Количество|");
+        printStream.println("|:-:|-:|");
+        List<Map.Entry<K, Integer>> sortedKeyValues = getSortedMap(mapCounter);
+        for (Map.Entry<K, Integer> keyValue : sortedKeyValues) {
+            printStream.println("|" + keyRenderer.apply(keyValue.getKey()) + "|" + keyValue.getValue() + "|");
+        }
+    }
+}
