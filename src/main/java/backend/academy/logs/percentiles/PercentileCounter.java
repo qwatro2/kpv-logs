6ead2c8f4edf9cf2d38ca88eb69b428fd68bcc58
qwatro2 <kv22828@gmail.com>
2024-11-01 21:23:28 +0300

    style: fix in PercentileCounters

--- src/main/java/backend/academy/logs/percentiles/PercentileCounter.java
+++ src/main/java/backend/academy/logs/percentiles/PercentileCounter.java
@@ -2,6 +2,8 @@ package backend.academy.logs.percentiles;
 
 public interface PercentileCounter {
     void reset(double targetPercentile);
+
     void processNext(int value);
+
     double getPercentile();
 }
--- src/main/java/backend/academy/logs/percentiles/TDigestPercentileCounter.java
+++ src/main/java/backend/academy/logs/percentiles/TDigestPercentileCounter.java
@@ -3,12 +3,14 @@ package backend.academy.logs.percentiles;
 import com.tdunning.math.stats.TDigest;
 
 public class TDigestPercentileCounter implements PercentileCounter {
+    private static final double COMPRESSION = 100.0;
+
     private TDigest tDigest;
     private double targetPercentile;
 
     @Override
     public void reset(double targetPercentile) {
-        this.tDigest = TDigest.createDigest(100.0);
+        this.tDigest = TDigest.createDigest(COMPRESSION);
         this.targetPercentile = targetPercentile;
     }
 
